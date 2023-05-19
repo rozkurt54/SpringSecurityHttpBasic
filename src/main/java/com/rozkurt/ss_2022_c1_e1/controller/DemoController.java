35923fb97ccd3d51c79ad57d9f0da0f2619b5681
Ramazan Özkurt <ramazanozkurt@gmail.com>
2023-05-19 16:25:08 +0300

    Some naming edited.

--- src/main/java/com/rozkurt/ss_2022_c1_e1/controller/DemoController.java
+++ src/main/java/com/rozkurt/ss_2022_c1_e1/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.rozkurt.ss_2022_c1_e1.conbtroller;
+package com.rozkurt.ss_2022_c1_e1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
